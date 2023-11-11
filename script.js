1de577edb0e4e81709f5cec0db8234c12913567f
GeSamaras <georgiosamaras@hotmail.com>
2023-11-11 21:46:13 +0100

    add book button functionality and display

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-// book object
-
+// the array that is going to hold all the book
+// objects, which will be looped through to display
 const myLibrary = [];
-let display = document.getElementsByClassName("display");
 
+
+// building the object constructor for the books
 function Book(title, author, pages, read) {
     // the constructor...
     console.log("Book constructor called");
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = function(){
-        if (read == true) {
-            return "Read";
-        } else {
-            return "Not Read";
-        }
-    };
+    this.read = read;
 }
 
 
@@ -23,35 +18,37 @@ function Book(title, author, pages, read) {
 
 
 function addBookToLibrary() {
-    // display book objects on page
-    for (let i = 0; i < myLibrary.length; i++) {
-        let book = myLibrary[i];
-        let title = book.title;
-        let author = book.author;
-        let pages = book.pages;
-        let read = book.read;
-        let bookCard = document.createElement("div");
-        bookCard.classList.add("bookCard");
-        bookCard.innerHTML = `
-            <div class="bookTitle">${title}</div>
-            <div class="bookAuthor">${author}</div>
-            <div class="bookPages">${pages}</div>
-            <div class="bookRead">${read}</div>
-        `;
-        display[0].appendChild(bookCard);
-    }
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read').checked;
+    let newBook = new Book(title, author, pages, read);
+
+    myLibrary.push(newBook);    
+    displayBooks();
 }
 
-addBookToLibrary();
 
-// user-made object example
-const book1 = new Book(
-    "The Hobbit",
-    "J.R.R. Tolkien", 
-    295, 
-    true
-);
+let newBookButton = document.querySelector("#add-book");
+newBookButton.addEventListener("click", function() {
+    let newBookForm = document.querySelector("#new-book-form");
+    newBookForm.style.display = "block";
+});
 
+document.querySelector("#submit").addEventListener("click", function(event) {
+    event.preventDefault();
+    let newBookForm = document.querySelector("#new-book-form");
+    newBookForm.style.display = "none";
+    addBookToLibrary();
+});
 
-// object.create
-//const book2 = Object.create(Book);
+function displayBooks() {
+    let libraryBook = document.querySelector("#library-book");
+    libraryBook.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookCard = document.createElement("div");
+        bookCard.innerHTML = `<p>${book.title}</p>`;
+        libraryBook.appendChild(bookCard);
+    }
+};
