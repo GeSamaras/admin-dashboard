13e456361379442ac18992df963847e0ba8aa30f
GeSamaras <georgiosamaras@hotmail.com>
2023-11-12 18:16:40 +0100

    its broken now teehee

--- script.js
+++ script.js
@@ -13,12 +13,47 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
+
+function toggleRead() {
+    myLibrary[index].toggleRead();
+    displayBooks()
+}
+
 
 //Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
 
 
+// function that will be called whenever "submit" is called,
+// rendering and creating all the html for the book blocks
+function displayBooks() {
+    let libraryBook = document.querySelector(".cards")
+    libraryBook.innerHTML = ""; //this is to make sure the books' blocks don't get duplicated after every submit
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookCard = document.createElement("div");
+        bookCard.innerHTML = `
+            <div class="card-header">
+                <h3 class="bookTitle">${book.title}</h3>
+                <h5 class="author">by ${book.author}</h5>
+            </div>
+            <div class="card-body">
+                <p>${book.pages} pages</p>
+                <p class="read-status">${book.read ? "Read" : "Not read"}</p>
+                <button class="remove-button" onclick="removeBook(${i})">Remove</button>
+                <button class="toggle-read-button" onclick="toggleRead(${i})">Toggle</button>
+            </div>              
+                `
+        libraryBook.appendChild(bookCard); // appending .card-blocks to .cards
+    }
+}
+
+
 function addBookToLibrary() {
-    let title = document.querySelector("#title").value;
+    let title = document.querySelector("#title").value; 
     let author = document.querySelector('#author').value;
     let pages = document.querySelector('#pages').value;
     let read = document.querySelector('#read').checked;
@@ -30,12 +65,12 @@ function addBookToLibrary() {
 
 
 let newBookButton = document.querySelector("#add-book");
-newBookButton.addEventListener("click", function() {
+    newBookButton.addEventListener("click", function() {
     let newBookForm = document.querySelector("#new-book-form");
     newBookForm.style.display = "block";
 });
 
-document.querySelector("#submit").addEventListener("click", function(event) {
+document.querySelector("#new-book-form").addEventListener("click", function(event) {
     event.preventDefault();
     let newBookForm = document.querySelector("#new-book-form");
     newBookForm.style.display = "none";
@@ -43,7 +78,7 @@ document.querySelector("#submit").addEventListener("click", function(event) {
 });
 
 function displayBooks() {
-    let libraryBook = document.querySelector("#library-book");
+    let libraryBook = document.querySelector(".card-blocks");
     libraryBook.innerHTML = "";
     for (let i = 0; i < myLibrary.length; i++) {
         let book = myLibrary[i];
@@ -52,3 +87,9 @@ function displayBooks() {
         libraryBook.appendChild(bookCard);
     }
 };
+
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    displayBooks();
+}
